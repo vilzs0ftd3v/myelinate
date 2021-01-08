f25aa4612f296470accf49578c94b9e7148141b9
vilzs0ftd3v <68156648+vilzs0ftd3v@users.noreply.github.com>
2021-01-08 08:20:08 +0800

    Add files via upload

---
+++ view/sidebar.php
@@ -0,0 +1,16 @@
+<div id = "toolBarContainer"><a href="#" id = "toolBar"><img id = "toolBarImage_id" src="public/images/menu.svg" alt="logo" width="30" height="24"></a></div>
+    <div class = "sidebar">
+      <ul>
+      <a href="dashboard"><img id = "logo_id" src="public/images/brain.svg" alt="logo" width="70" height="60" style="color:white;margin-left:42px;margin-top:30px;"></a>
+          <!-- <li id = "logoContainer-open" class="logoContainerClass" style=" margin-top:30px;margin-left:0px;  list-style: none;"><a href="dashboard"><img id = "logo_id" src="public/images/brain.svg" alt="logo" width="70" height="60" style="color:white;margin-left:42px;margin-top:6px;"></a></li>  -->
+          
+          <li style=" margin-top: 20px;"><a href="question" class ="sidebar-a" id = "question_id"><img class = "menu-a" src="public/images/question.svg" alt="logo" width="30" height="24">&nbsp;<label id = "question_menu" style="margin-left:8px; font-size: 15px;"><small>Questions</small></label></a></li>
+          <li><a class ="sidebar-a act" href="note" id = "archive_id"><img class = "menu-a" src="public/images/note.svg" alt="logo" width="30" height="24">&nbsp;<label id = "archive_menu" style="margin-left:8px; font-size: 15px;"><small>Note</small></label></a></li>
+          <li><a class ="sidebar-a" href="activity" id = "activity_id"><img class = "menu-a" src="public/images/activity.svg" alt="logo" width="30" height="24">&nbsp;<label id = "activity_menu" style="margin-left:8px; font-size: 15px;"><small>Activity</small></label></a></li>
+          <!-- <li><a class ="sidebar-a" href="reward"id = "reward_id"><img class = "menu-a" src="public/images/gift.svg" alt="logo" width="30" height="24">&nbsp;<label id = "reward_menu" style="margin-left:8px; font-size: 15px;"><small>Reward</small></label></a></li> -->
+          <li><a class ="sidebar-a" href="reward"id = "reward_id"><img class = "menu-a" src="public/images/gift.svg" alt="logo" width="30" height="24">&nbsp;<label id = "reward_menu" style="margin-left:8px; font-size: 15px;"><small>Points</small></label></a></li>
+          <li><a class ="sidebar-a" style='border-bottom:1px solid #fd7e14;' href="<?php echo URL?>logout" id = "logout_id"><img class = "menu-a" src="public/images/log-out.svg" alt="logo" width="30" height="24">&nbsp;<label id = "out_menu" style="margin-left:8px; font-size: 15px;"><small>Log-out</small></label></a></li>
+
+      </ul>
+    </div>
+ </div>
