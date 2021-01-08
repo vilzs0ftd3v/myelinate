b88d4a890c569dcc5b06d60480dac21f1fa616ad
vilzs0ftd3v <68156648+vilzs0ftd3v@users.noreply.github.com>
2021-01-08 08:14:05 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+
+//define("URL", "http://localhost/project/myelination1.0/");
+define("URL", "https://myelinate.herokuapp.com/");
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include_once("config.php");
+include_once("lib/controller.php");
+include_once("lib/app.php");
+include_once("lib/view.php");
+include_once("lib/session.php");
+
+
+
+$app = new App();
+$app->init();
