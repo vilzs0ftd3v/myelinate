1f3878c099f7fef67eaab5b8b2d0dee54711a935
vilzs0ftd3v <68156648+vilzs0ftd3v@users.noreply.github.com>
2021-01-11 01:44:48 +0800

    Update found.php

--- view/found.php
+++ view/found.php
@@ -2,5 +2,5 @@
  	
     <br><br><br>
     <!-- <p style="text-align: center;color:red;">Sorry, you need to <a href="http://localhost/project/myelination1.0/login">login</a> first in order for you to access this page.</p>  -->
-    <p style="text-align: center;color:red;">Sorry, you need to <a href="http://blankknalb.0fees.us/login">login</a> first in order for you to access this page.</p>
+    <p style="text-align: center;color:red;">Sorry, you need to <a href="https://myelinate.herokuapp.com/login">login</a> first in order for you to access this page.</p>
 <?php endif; ?>
